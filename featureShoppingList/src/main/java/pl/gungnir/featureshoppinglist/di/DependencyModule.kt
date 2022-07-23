3ef7119eb7aea6f27a2dd02272e59f9f5fa6c534
RowickaM <rowickam.23@gmail.com>
2022-07-23 21:07:32 +0200

    #11 move dependency injection to modules

---
+++ featureShoppingList/src/main/java/pl/gungnir/featureshoppinglist/di/DependencyModule.kt
@@ -0,0 +1,21 @@
+package pl.gungnir.featureshoppinglist.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import pl.gungnir.database.domain.repository.BuyListRepository
+import pl.gungnir.featureshoppinglist.useCase.GetBuyListUseCase
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class DependencyModule {
+
+    @Singleton
+    @Provides
+    fun provideGetBuyListUseCase(repository: BuyListRepository): GetBuyListUseCase {
+        return GetBuyListUseCase(repository)
+    }
+
+}
